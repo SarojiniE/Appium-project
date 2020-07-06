3875230eb7e6c056636d0be4c903252811fe0465
SarojiniEmekar <SarojiniEmekar@9.102.48.99>
2020-07-06 16:41:05 +0530

    Udemy and Amazon test cases created from Mobile Testing assignments

--- src/browser/AmazonAddProductToCart.java
+++ src/browser/AmazonAddProductToCart.java
@@ -4,9 +4,14 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.Select;
+
+import com.google.common.collect.ImmutableMap;
 
 import io.appium.java_client.MobileBy;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
@@ -14,7 +19,7 @@ import io.appium.java_client.remote.MobileCapabilityType;
 
 public class AmazonAddProductToCart {
 
-	public static void main(String[] args) throws MalformedURLException {
+	public static void main(String[] args) throws MalformedURLException, InterruptedException {
 		DesiredCapabilities cap = new DesiredCapabilities();
 		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "abc");
 		cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
@@ -27,7 +32,19 @@ public class AmazonAddProductToCart {
 		
 		driver.findElement(MobileBy.xpath("//input[@type='text']")).sendKeys("iphone 7s plus");
 		driver.findElement(MobileBy.xpath("//div[@class='suggest_link2']")).click();
+		driver.findElement(MobileBy.xpath("//span[text()='Apple iPhone 7 (32GB) - Black']")).click();
+		driver.findElement(MobileBy.xpath("//*[@class='a-size-base a-color-base a-text-normal']")).click();
+		driver.findElement(MobileBy.xpath("//*[@class='a-sheet-close a-icon a-icon-close-white a-focus-hidden']")).click();
 		
+		Thread.sleep(3000);
+		
+		driver.findElement(MobileBy.xpath("//*[@class='a-icon a-icon-supplemental']/following-sibling::input")).click();
+		driver.findElement(MobileBy.xpath("(//*[@class='a-button-input' and @type='submit'])[9]")).click();
+		driver.findElement(MobileBy.xpath("//*[@class='a-sheet-close a-focus-hidden']")).click();
+		driver.findElement(MobileBy.xpath("//*[@class='a-button-text a-declarative']")).click();
+		driver.findElement(MobileBy.xpath("//*[@id='mobileQuantityDropDown_4']")).click();
+		driver.findElement(MobileBy.xpath("//*[@class='a-button-text a-text-left']")).click();
+
 		
 	}
 
